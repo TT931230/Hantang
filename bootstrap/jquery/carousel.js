7a209937de7c1968d8a3cdea92e1c3aa12d350ba
FaJingyi <jingyi.fa@gmail.com>
2016-01-13 21:14:54 +0800

    css -3

---
+++ bootstrap/jquery/carousel.js
@@ -0,0 +1,18 @@
+$(function () {
+
+  // Slideshow 4
+  $("#carousel").responsiveSlides({
+	auto: false,
+	pager: false,
+	nav: true,
+	speed: 500,
+	namespace: "callbacks",
+	before: function () {
+	  $('.events').append("<li>before event fired.</li>");
+	},
+	after: function () {
+	  $('.events').append("<li>after event fired.</li>");
+	}
+  });
+
+  });
---
+++ bootstrap/jquery/carousel_home.js
@@ -0,0 +1,56 @@
+$(function () {
+
+  // Slideshow 4
+  $("#carousel1").responsiveSlides({
+	auto: false,
+	pager: false,
+	nav: true,
+	speed: 500,
+	namespace: "callbacks",
+	before: function () {
+	  $('.events').append("<li>before event fired.</li>");
+	},
+	after: function () {
+	  $('.events').append("<li>after event fired.</li>");
+	}
+  });
+  $("#carousel2").responsiveSlides({
+	auto: false,
+	pager: false,
+	nav: true,
+	speed: 500,
+	namespace: "callbacks",
+	before: function () {
+	  $('.events').append("<li>before event fired.</li>");
+	},
+	after: function () {
+	  $('.events').append("<li>after event fired.</li>");
+	}
+  });
+    $("#carousel3").responsiveSlides({
+	auto: false,
+	pager: false,
+	nav: true,
+	speed: 500,
+	namespace: "callbacks",
+	before: function () {
+	  $('.events').append("<li>before event fired.</li>");
+	},
+	after: function () {
+	  $('.events').append("<li>after event fired.</li>");
+	}
+  });
+    $("#carousel4").responsiveSlides({
+	auto: false,
+	pager: false,
+	nav: true,
+	speed: 500,
+	namespace: "callbacks",
+	before: function () {
+	  $('.events').append("<li>before event fired.</li>");
+	},
+	after: function () {
+	  $('.events').append("<li>after event fired.</li>");
+	}
+  });
+  });
