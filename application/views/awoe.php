dae42ae885fb0810ec0786c01c782ca65a287730
FaJingyi <jingyi.fa@gmail.com>
2016-01-20 23:02:04 +0800

    css [ update relativevideo]

--- application/views/awoe.php
+++ application/views/awoe.php
@@ -50,7 +50,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                             <div class="uldetails">
                                 {locationdetails}
                                 <a href="javascript:;" onclick="$searchrelatedvideo('{id}')">
-                                <span class="musictypedetail" >{keyword}</span>
+                                <span class="musictypedetail2" >{keyword}</span>
                                 </a>
                                 {/locationdetails}
                             </div>
