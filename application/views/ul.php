a39053066484449e067a558f120376529cf6118e
FaJingyi <jingyi.fa@gmail.com>
2016-01-21 12:43:29 +0800

    css - need to update

--- application/views/ul.php
+++ application/views/ul.php
@@ -50,7 +50,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                         </div>
                         <div class="diftype">
                             <div class="ultypes">{locationtype}</div>
-                            <div class="uldetails">
+                            <div class="uldetails2">
                                 {locationdetails}
                                 <a href="javascript:;" onclick="$searchrelatedvideo('{id}')">
                                 <span class="musictypedetail2" >{keyword}</span>
