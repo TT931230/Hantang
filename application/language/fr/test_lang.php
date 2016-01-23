5c5eb201f73249201baa3d1bd1168acafc3190f9
FaJingyi <jingyi.fa@gmail.com>
2016-01-23 21:35:28 +0800

    css ulinner finish

--- application/language/fr/test_lang.php
+++ application/language/fr/test_lang.php
@@ -5,24 +5,24 @@
  * Date: 2016/1/12
  * Time: 14:33
  */
-$lang['commit']='commit';
-$lang['inputtip']='Please input the keyword';
-$lang['seniorsearch']='高级搜索';
-$lang['search']='search';
-$lang['language']='Language';
-$lang['introduce']='公司简介';
-$lang['platform']='播出平台';
-$lang['partner']='合作伙伴';
+$lang['commit']='Vérifier';
+$lang['inputtip']='Saisie de mots clefs';
+$lang['seniorsearch']='Recherche avancée';
+$lang['search']='De recherche';
+$lang['language']='langue';
+$lang['introduce']=' Introduction';
+$lang['platform']=' Plate-forme de diffusion';
+$lang['partner']='Partenaires';
 $lang['home']='Page d’accueil';
-$lang['about']='À propos de nous';
-$lang['ul']='《极致》';
-$lang['awoe']='《问鼎世界》';
-$lang['music']='国际音乐节';
-$lang['join']='加入汉唐';
-$lang['brandname']='品牌名称';
-$lang['type']='类型分类';
-$lang['location']='地点';
-$lang['keyword']='关键字';
-$lang['time']='时间';
-$lang['reset']='重新筛选';
-$lang['dogandhorse']='声色犬马';
+$lang['about']='Pour nous suivre';
+$lang['ul']='The Ultimate Luxury ';
+$lang['awoe']='A World of Excellence';
+$lang['music']='Festival de musique Hantang';
+$lang['join']='Pour nous joindre';
+$lang['brandname']='Nom de marque';
+$lang['type']='Le type de classification';
+$lang['location']='Lieu';
+$lang['keyword']='Mots - clés';
+$lang['time']='Le temps';
+$lang['reset']='Re de criblage';
+$lang['dogandhorse']='Salut';
