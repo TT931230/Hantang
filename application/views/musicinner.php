7ee2519083900078aec90d001276dddc5ac38a3c
FaJingyi <jingyi.fa@gmail.com>
2016-01-24 11:40:58 +0800

    adjust inner

--- application/views/musicinner.php
+++ application/views/musicinner.php
@@ -26,19 +26,51 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                     </video>
                 </div>
            
+        
             <div class="subtitle">
-                <div>
-                    <div>
+                <div class="ulinnera">
+                    <div class="innertexttitle">
+                        {source_name}
+                    </div>
+                    <div class="innertext">
                         {source_remark}
                     </div>
                 </div>
-                <div>
-                    {imagearea2}
-                        <img src="{source_location}">
-                    {/imagearea2}
+                <div class="ulinnerc">
+
+
+                    <div class="part2">
+                        <div>
+                            <a href="{source_url}" download="{source_name}">
+
+                                <img id="dload" alt="" src="../../bootstrap/images/dload.png"/>
+                            </a>
+
+                        </div>
+                        <div class="ul_des">下载</div>
+                    </div>
+                    <div class="part3">
+                        <div>
+                            <img id="phone" alt="" src="../../bootstrap/images/dload.png"/>
+
+                        </div>
+                        <div class="ul_des">手机</div>
+                    </div>
+                </div>
+                {/video}
+                <div class="ulinnerb">
+                    <div id="share">
+                        <div class="bdsharebuttonbox">
+                            <a href="#" class="bds_weixin" data-cmd="weixin" title="分享到微信"></a>
+                            <a href="#" class="bds_tsina" data-cmd="tsina" title="分享到新浪微博"></a>
+                            <a href="#" class="bds_qzone" data-cmd="qzone" title="分享到QQ空间"></a>
+                            <a href="#" class="bds_sqq" data-cmd="sqq" title="分享到QQ好友"></a>
+                        </div>
+                    </div>
+                    <div id="sharetext">分享给好友</div>
                 </div>
             </div>
-            {/video}
+                
             <div class="videoslug">
                 相关视频
             </div>
