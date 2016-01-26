a98b3d1303b8238bdd84be2ffb652d0a68a51775
FaJingyi <jingyi.fa@gmail.com>
2016-01-26 13:49:25 +0800

    css manage

--- application/views/videoexaminelist.php
+++ application/views/videoexaminelist.php
@@ -6,15 +6,16 @@
  * Time: 15:17
  */defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
-<div>
-    <div class="row">
+<div class="managepagetitle">视频审核列表</div>
+<div class="hpm-div">
+    <div class="row ce-div">
         <label class="col-lg-2">视频明细列表</label>
     </div>
-    <div class="row">
+    <div class="row ce-div">
         <label class="col-lg-2">所属栏目</label>
         <input class="col-lg-2" type="text">
     </div>
-    <div class="row">
+    <div class="row ">
         <button type="submit" class="btn btn-default">查询</button>
     </div>
 </div>
