847d704533b439914617d678ed510a6843e66c98
FaJingyi <jingyi.fa@gmail.com>
2016-01-28 21:07:17 +0800

    update music inner

--- application/views/departmentlist.php
+++ application/views/departmentlist.php
@@ -12,23 +12,23 @@
        <label class="col-lg-2">视频明细列表</label>
    </div> -->
 <div id="caselistshow" class="cl-table">
-    <table>
+    <table style="width:100%;">
         <tr>
-            <td class="vl-title1">部门名称</td>
-            <td class="vl-title2">部门状态</td>
-            <td class="vl-title3">部门顺序</td>
-            <td class="vl-title7">编辑</td>
+            <td class="dptl-title1">部门名称</td>
+            <td class="dptl-title2">部门状态</td>
+            <td class="dptl-title3">部门顺序</td>
+            <td class="dptl-title4">编辑</td>
         </tr>
         {departmentselect}
-        <tr>
-            <td id="{id}" class="vl-imgname">{department}</td>
-            <td id="{id}_status" class="vl-imgadd">
+        <tr style="width:100%;">
+            <td id="{id}" class="dptl-name">{department}</td>
+            <td id="{id}_status" class="dptl-state">
                 {status}
             </td>
-            <td class="vl-imgseq">
+            <td class="dptl-seq">
                 <input type="number" value="{sequence}" name="sequence" id="{id}_sequence" class="vl-imgseqinput">
             </td>
-            <td id="{id}_edit" class="vl-imgedit">
+            <td id="{id}_edit" class="dptl-edit">
                 <a href="javascript:;" onclick="$updatedepartment('{id}') " class="cl-imgeditbtn">保存</a>
                 <a href="javascript:;" onclick="$changedepartmentstatus('{id}')" class="cl-imgeditbtn">隐藏/显示</a>
                 <a href="javascript:;" onclick="$deletedepartment('{id}')" class="cl-imgeditbtn">删除</a>
