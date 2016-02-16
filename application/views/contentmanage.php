25fe31ed5878c150ac24b4fc3e6717c2a9db75f0
Mark Shi <13795260699@163.com>
2016-02-16 21:02:33 +0800

    multiselect

--- application/views/contentmanage.php
+++ application/views/contentmanage.php
@@ -12,37 +12,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
 
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
 
     <title>后台管理</title>
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.9.4/css/bootstrap-select.min.css">
-
-
-
     <!-- Bootstrap Core CSS -->
-    <link href="manage/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
+    <script type="text/javascript" src="http://www.daimajiayuan.com/download/jquery/jquery-1.10.2.min.js"></script>
+    <script type="text/javascript" src="manage/bootstrap-select.js"></script>
+    <link rel="stylesheet" type="text/css" href="manage/bootstrap-select.css">
+    <link href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
+    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
     <link href="manage/css/sb-admin.css" rel="stylesheet">
-
-    <!-- Morris Charts CSS -->
-    <link href="manage/css/plugins/morris.css" rel="stylesheet">
-
-    <!-- Custom Fonts -->
     <link href="manage/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-
-
 
 </head>
 
@@ -187,7 +171,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         </div>
         <!-- /.navbar-collapse -->
     </nav>
-
     <div id="page-wrapper" style="height: auto">
         <div class="container-fluid">
 
@@ -196,26 +179,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     </div>
     <!-- /#page-wrapper -->
 </div>
-<!-- /#wrapper -->
 
-<!-- jQuery -->
-<script src="manage/js/jquery.js"></script>
-<!-- Bootstrap Core JavaScript -->
-<script src="manage/js/bootstrap.min.js"></script>
-<!-- Morris Charts JavaScript -->
 <script src="manage/js/plugins/morris/raphael.min.js"></script>
-<script src="manage/js/manage.js"></script>
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.9.4/js/bootstrap-select.min.js"></script>
-
-<!-- (Optional) Latest compiled and minified JavaScript translation files -->
-<script src="manage/js/js/i18n/defaults-en_US.min.js"></script>
-
-
-
-
 <script src="manage/js/plugins/morris/morris.min.js"></script>
 <script src="manage/js/plugins/morris/morris-data.js"></script>
+<script src="manage/js/manage.js"></script>
 </body>
 </html>
 
